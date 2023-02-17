714b230096e8ed2dee700ab15ee8073c7ffdb58a
Kshitij Chaurasiya <kshitij.dean.sam@gmail.com>
2023-02-17 18:22:32 +0530

    fix: webpack function resolution issue

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/index.ts',
-	mode: 'production',
+	mode: 'development',
+	target: 'web',
 	devtool: 'source-map',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+		libraryTarget : "umd"
 	},
 	resolve: {
 		extensions: ['.ts', '.js'],
