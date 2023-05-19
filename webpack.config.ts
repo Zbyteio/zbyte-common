0803b96159c90bf769858777ff95e70fc9d60fd9
Kshitij Chaurasiya <kshitij.dean.sam@gmail.com>
2023-05-19 14:43:59 +0530

    Add globalObject flag to solve next integration issue

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,7 +8,8 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-		libraryTarget : "umd"
+		libraryTarget : "umd",
+		globalObject: "this"
 	},
 	resolve: {
 		extensions: ['.ts', '.js'],
